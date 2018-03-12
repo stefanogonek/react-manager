9a86c0246274dd75e455f8fa63b35f7cebabb9d2
Stefan Ogonek <stefan.ogonek@gmail.com>
2018-03-12 10:26:58 +0100

    lecture 120

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import firebase from 'firebase';
-import { EMAIL_CHANGED, PASSWORD_CHANGED } from './types';
+import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS } from './types';
 
 export const emailChanged = (text) => ({
   type: EMAIL_CHANGED,
@@ -11,9 +11,9 @@ export const passwordChanged = (text) => ({
   payload: text
 });
 
-export const loginUser = ({ email, password }) => {
-  return (dispatch) => {
-    firebase.auth().signInWithEmailAndPassword(email, password)
-      .then(user => console.log(user));
-  };
+export const loginUser = ({ email, password }) => (dispatch) => {
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(user => {
+      dispatch({ type: LOGIN_USER_SUCCESS, payload: user });
+    });
 };
