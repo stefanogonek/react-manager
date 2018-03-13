e344624d48ad9d2ecc5f02ec255a52381c56f6ff
Stefan Ogonek <stefan.ogonek@gmail.com>
2018-03-13 14:37:56 +0100

    lecture 129

---
+++ src/components/EmployeeList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class EmployeeList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+      </View>
+    );
+  }
+}
+
+export default EmployeeList;
