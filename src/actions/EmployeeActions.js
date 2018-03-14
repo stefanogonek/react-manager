4fc13ee6eb4eca65d0f2016d4fbceb851def775e
Stefan Ogonek <stefan.ogonek@gmail.com>
2018-03-14 20:07:44 +0100

    lecture 137

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -2,9 +2,7 @@ import {
   EMPLOYEE_UPDATE
 } from './types';
 
-export const employeeUpdate = ({ prop, value }) => {
-  return {
-    type: EMPLOYEE_UPDATE,
-    payload: { prop, value }
-  };
-};
+export const employeeUpdate = ({ prop, value }) => ({
+  type: EMPLOYEE_UPDATE,
+  payload: { prop, value }
+});
